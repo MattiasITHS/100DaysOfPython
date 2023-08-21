0a2d44615fcee8b7eec7e7039a804e9da16d2c50
MattiasITHS <hovde__@hotmail.com>
2023-08-21 15:15:26 +0200

    Adding course solutions

--- Day-18-Turtle/Day_18_Turtle_RandomWalk.py
+++ Day-18-Turtle/Day_18_Turtle_RandomWalk.py
@@ -1,3 +1,4 @@
+import turtle
 from turtle import Turtle, Screen
 import random
 
@@ -8,9 +9,16 @@ colours = ["Blue", "SeaGreen", "Black", "Red", "Yellow", "SlateGrey"]
 direction = [0, 90, 180, 270]
 tim.pensize(10)
 tim.speed(50)
+turtle.colormode(255)
+def random_color():
+    r = random.randint(0, 255)
+    b = random.randint(0, 255)
+    g = random.randint(0, 255)
+    random_color = (r, b, g)
+    return random_color
 
 for _ in range(200):
-    tim.color(random.choice(colours))
+    tim.color(random_color())
     tim.forward(20)
     tim.setheading(random.choice(direction))
 
