d1e8608b796e94d7636e5ddf08b94adf5ad1988b
MattiasITHS <hovde__@hotmail.com>
2023-09-11 12:02:07 +0200

    Updates

---
+++ Day_8_ceasarCipher.py
@@ -0,0 +1,33 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+
+            # Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+            # e.g. start_text = "meet me at 3"
+            # end_text = "•••• •• •• 3"
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+
+yes_no = True
+while yes_no:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    yes_no = input("Do you want to continue? type yes or no")
+    if yes_no.lower() == "no":
+        yes_no = False
